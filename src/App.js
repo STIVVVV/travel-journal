27141e88e26d9ebde6bc896456cd1e18e5db506f
STIVVVV <stevencanfield2000@gmail.com>
2022-05-26 00:15:53 -0400

    started setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
 }
-
-export default App;
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-Navbar ()
-{
-    <nav>
-        <img></img>
-    </nav>
+export default function Navbar() 
+ {
+    return (
+        <nav>
+            <img className="nav--logo" src="/images/world.png" alt="world"/>
+            <h1 className="nav--logo">my travel journal.</h1>
+        </nav>
+    )
 }
